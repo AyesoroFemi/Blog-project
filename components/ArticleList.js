7294fa9ffd42aecfa17d69d32661023fb0b013ef
Decagon <decagon@Femi-Decagons-MacBook-Air.local>
2021-11-03 06:54:50 +0100

    fix: blog-project

---
+++ components/ArticleList.js
@@ -0,0 +1,32 @@
+// import ArticleItem from './ArticleItem'
+// import articleStyles from '../styles/Article.module.css'
+
+
+// function ArticleList({articles}) {
+//     return (
+//         <div className={articleStyles.grid}>
+            
+//             {articles.map((article) => (
+//                 <ArticleItem article={article} />
+//             ))}
+     
+//         </div>
+//     )
+// }
+
+// export default ArticleList
+
+import ArticleItem from './ArticleItem'
+import articleStyles from '../styles/Article.module.css'
+
+const ArticleList = ({ articles }) => {
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article) => (
+        <ArticleItem article={article} />
+      ))}
+    </div>
+  )
+}
+
+export default ArticleList
---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Nav from './Nav'
+import Header from './Header'
+import styles from '../styles/Layout.module.css'
+
+function Layout({children}) {
+    return (
+        <>
+            <Nav />
+            <Header />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default Layout

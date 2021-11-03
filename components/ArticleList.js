d317e84e180244e431ce775585246bcfebde079c
Decagon <decagon@Femi-Decagons-MacBook-Air.local>
2021-11-03 07:10:07 +0100

    key added

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -23,7 +23,7 @@ const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-        <ArticleItem article={article}  key={article.id} />
+        <ArticleItem article={article} key={article.id}/>
       ))}
     </div>
   )
